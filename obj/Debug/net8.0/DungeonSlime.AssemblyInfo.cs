93b5102cd345166010145b940c47d258191edf53
rajanchoksi79 <164361171+rajanchoksi79@users.noreply.github.com>
2025-08-02 17:29:09 +0530

    class library created and tested afterwards

--- obj/Debug/net8.0/DungeonSlime.AssemblyInfo.cs
+++ obj/Debug/net8.0/DungeonSlime.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DungeonSlime")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c13039ccfdf15b34ea49ffa1582e9bf321ab74fd")]
 [assembly: System.Reflection.AssemblyProductAttribute("DungeonSlime")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DungeonSlime")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
